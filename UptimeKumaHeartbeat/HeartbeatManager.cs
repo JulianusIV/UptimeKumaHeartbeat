036eed799664b24078249f881608c5c0a304a100
JulianusIV <ivjulianus@gmail.com>
2023-02-02 22:21:34 +0100

    Add project files.

---
+++ UptimeKumaHeartbeat/HeartbeatManager.cs
@@ -0,0 +1,61 @@
+﻿namespace UptimeKumaHeartbeat
+{
+    public class HeartbeatManager : IDisposable
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+        PeriodicTimer? _heartbeatTimer;
+
+        public static async Task SendSingleHeartBeatAsync(string targetUrl, HeartbeatData data)
+        {
+            try
+            {
+                using var client = new HttpClient();
+
+                var requestUri = new Uri($"{targetUrl}?status={data.Status}&msg={data.Message}&ping={(data.Ping is null ? string.Empty : ((int)data.Ping).ToString())}");
+
+                await client.GetAsync(requestUri);
+            }
+            catch (Exception)
+            {
+                //probably just connection issues, so we likely got bigger problems right here
+            }
+        }
+
+        public Task StartHeartbeatsAsync(string targetUrl, HeartbeatData data, CancellationToken? cancellationToken = null, int interval = 60000)
+        {
+            CancellationToken token = cancellationToken is null ? _cancellationTokenSource.Token : (CancellationToken)cancellationToken;
+
+            _ = Task.Factory.StartNew(async () =>
+            {
+                _heartbeatTimer = new(TimeSpan.FromMilliseconds(interval));
+                do
+                    await SendSingleHeartBeatAsync(targetUrl, data);
+                while (await _heartbeatTimer.WaitForNextTickAsync(token));
+
+            }, TaskCreationOptions.LongRunning);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _heartbeatTimer?.Dispose();
+            _cancellationTokenSource.Cancel();
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public class HeartbeatData
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public int? Ping { get; set; }
+
+        public HeartbeatData(string status, string message, int? ping = null)
+        {
+            Status = status;
+            Message = message;
+            Ping = ping;
+        }
+    }
+}
---
+++ UptimeKumaHeartbeatTests/HeartbeatManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UptimeKumaHeartbeat.Tests
+{
+    [TestClass]
+    public class HeartbeatManagerTests
+    {
+        [TestMethod]
+        public void SendSingleHeartBeatAsyncTest()
+        {
+            HeartbeatManager.SendSingleHeartBeatAsync("https://status.julianusiv.de/api/push/uCf8Z9Oc02", new("", "")).GetAwaiter().GetResult();
+        }
+
+        [TestMethod]
+        public void StartHeartbeatsAsyncTests()
+        {
+            CancellationTokenSource cancellationTokenSource = new();
+
+            using HeartbeatManager heartbeatManager = new();
+            HeartbeatData data = new("", "");
+            var task = heartbeatManager.StartHeartbeatsAsync("https://status.julianusiv.de/api/push/uCf8Z9Oc02", data, cancellationTokenSource.Token);
+
+            cancellationTokenSource.CancelAfter(TimeSpan.FromMinutes(3));
+            data.Message = "test";
+            data.Ping = 1000;
+
+            cancellationTokenSource.Token.WaitHandle.WaitOne();
+            if (task.Exception is not null)
+                Assert.Fail();
+            Assert.IsTrue(task.IsCompletedSuccessfully);
+        }
+    }
+}
